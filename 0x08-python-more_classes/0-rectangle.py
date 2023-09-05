580e01d5098a76fd9306a047c7362e01d35ce3d2
Taki-Abde <takiabderrahim005@gmail.com>
2023-09-05 14:17:30 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+Contains class Rectangle
+Empty class
+by: abderrahim
+"""
+
+
+class Rectangle():
+    """
+    Defines a class empty  rectangle
+    """
+
+    pass
