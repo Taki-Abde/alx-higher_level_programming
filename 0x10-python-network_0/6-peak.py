27411d21dfd125154b3be2685a3cfdaf19c07f3e
Taki-Abde <takiabderrahim005@gmail.com>
2024-01-25 13:16:39 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,34 +1,11 @@
 #!/usr/bin/python3
 # made by ABDE
-'''
-Returns the peak of an unsorted lst
-'''
-ret = int or None
+#!/usr/bin/python3
+"""Algorithms for list of integers."""
 
 
-def find_peak(list_of_integers) -> ret:
-    '''
-    Finds a numb that is greater than both left & right
-    '''
-
-    listLen = len(list_of_integers)
-    if listLen == 0:
-        return None
-
-    tmp = list_of_integers
-    i = 0
-    n = listLen - 1
-
-    if tmp[i] > tmp[i+1]:
-        return tmp[i]
-    if tmp[n] > tmp[n-1]:
-        return tmp[n]
-
-    x = (i + n) // 2
-    if tmp[x-1] < tmp[x] and tmp[x+1] < tmp[x]:
-        return tmp[x]
-    if tmp[x] < tmp[x-1]:
-        return find_peak(tmp[i:x+1])
-    if tmp[x] < tmp[x+1]:
-        return find_peak(tmp[x:n+1])
-    return tmp[i]
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
